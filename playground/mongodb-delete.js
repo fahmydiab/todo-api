410c6cd2019d7d32493875e965a898ef0fd372dc
Fahmy Diab <king.diab.007@gmail.com>
2019-07-16 13:07:29 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+    if(err){
+        console.log('Unable to connect to MongoDB server');
+    }
+        console.log('Connected to MongoDB server');
+        
+        //delete many
+        // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result)=>{
+        //     console.log(result);
+        // });
+        
+        //delete one
+        // db.collection("Todos").deleteOne({text:"Eat lunch"}).then((result)=>{
+        //       console.log(result);
+        //   });
+        
+        //find one and delete
+        // db.collection("Todos").findOneAndDelete({completed: false}).then((result)=>{
+        //     console.log(result);
+        // }); 
+
+        //delete many
+        db.collection('Users').deleteMany({name:"Fahmy"});
+        
+        //delete one
+        // db.collection("Todos").deleteOne({text:"Eat lunch"}).then((result)=>{
+        //       console.log(result);
+        //   });
+        
+        //find one and delete
+        // db.collection("Users").findOneAndDelete({_id: new ObjectID("5d2b4b15eb9beb0e50247416")}).then((result)=>{
+        //     console.log(JSON.stringify(result, undefined, 2));
+        // }); 
+        db.close();
+});
